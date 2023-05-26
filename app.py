dccc6f58404cf3365b313772925bb411266f9025
MarcoPfattischer <marco.pfattischer@telenot.de>
2023-05-26 14:34:40 +0200

    configured host and port

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
+port = 1234
+host = '127.0.0.1'
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/helloworld')
 def hello_world():  # put application's code here
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=host, port=port)
