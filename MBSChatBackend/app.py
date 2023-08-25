2033e72d203e2fb640668d62e805b715908a01ff
MarcoPfattischer <marco.pfattischer@telenot.de>
2023-08-25 15:54:51 +0200

    Added message sending to backend

--- MBSChatBackend/app.py
+++ MBSChatBackend/app.py
@@ -4,18 +4,22 @@ port = 1234
 host = '127.0.0.1'
 app = Flask(__name__)
 
+message = 'No messages'
 
 @app.route('/title')
 def hello_world():  # put application's code here
     return {
-        "title": "Hello World the new JSON Backend",
+        "title": message,
     }
 
 @app.route('/title', methods=['POST'])
-def post_example():    
+def post_example():
+    global message
     data = request.json
     if request.is_json:
-        print (data)
+        message = data["message"]
+        print(data)
+        print(message)
     else:
         print ('not json')
     return data
