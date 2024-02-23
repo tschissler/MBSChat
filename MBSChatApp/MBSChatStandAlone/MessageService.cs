624e77b552d128d7445758a37471a643211fdbc5
Joachim Blank <joachim.blank@witec.de>
2024-02-23 15:36:46 +0100

    Notification für Messages implementiert

--- MBSChatApp/MBSChatStandAlone/MessageService.cs
+++ MBSChatApp/MBSChatStandAlone/MessageService.cs
@@ -3,4 +3,12 @@
 public class MessageService
 {
     public List<string> Messages = new();
+
+    public void AddMessage(string aMessage)
+    {
+        Messages.Add(aMessage);
+        Notify?.Invoke();
+	}
+
+    public event Action? Notify;
 }
